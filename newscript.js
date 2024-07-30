0568c3bce0d9028cf9894172f2141ec05dbb5b28
aparna12350 <aparnaushas@gmail.com>
2024-07-30 20:10:21 +0530

    welcome

---
+++ newscript.js
@@ -0,0 +1,84 @@
+// console.log("my first code");
+// comment
+/*var a=5;
+console.log(a);
+let b=50;
+console.log(b);
+{
+    var a=34;
+    console.log(a);
+}
+var a=10;
+console.log(a);
+let car=new Object();
+car.cName="i20"
+car.cColor="White"
+console.log(car);
+//function def
+function display(fname)
+{
+console.log('welcome'+fname+'tofunctions');
+}
+display('aparna');//function call
+function Add(a,b)
+{
+    var sum=a+b;
+   // console.log(sum);
+   return sum;
+}
+var result=Add(10,25);
+console.log(result);
+
+function Multiplication(a,b)
+{
+    var mul=a*b;
+    return mul;
+}
+var res=Multiplication(5,10);
+console.log('the result is'+ res);
+if (res>0) {
+    console.log('vale is greater than 0')
+    
+} else {
+    console.log('value s less than 0')
+    
+}
+    */
+//looping
+//print 1 to 10
+/*
+for (let i = 0; i <10 ; i++) {
+    console.log(i+1);
+    
+}
+    
+var a1=[1,45,34,56,78]
+for (let i=0;i<a1.length;i++)
+{
+    console.log(a1[i])
+}
+    
+   
+   
+   var obj={fname:'anu',age:20}
+
+   for (const i in obj)
+   {
+    console.log(obj[i])
+
+   }
+   
+for (const i of obj) {
+    console.log(i);
+    
+}*/
+let i=0;
+while (i<10) {
+    console.log(i+1)
+i++
+}
+let r=1;
+do{
+    console.log(r);
+    r++
+}while(r<=10)
---
+++ sample.js
@@ -0,0 +1,3 @@
+console.log('john');
+console.log(20);
+console.log('student');
